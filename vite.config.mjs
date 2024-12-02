a84687df61005af2b8d67cae37e5273d249852b1
kayibea <gatse1531@gmail.com>
2024-12-02 07:47:34 +0100

    🔧 update config file

--- vite.config.mjs
+++ vite.config.mjs
@@ -25,7 +25,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
