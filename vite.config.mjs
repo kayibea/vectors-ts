aa34f7c75e4437e0103dc824dc8aeabb4eec164f
kayibea <gatse1531@gmail.com>
2024-12-02 07:26:39 +0100

    🔧 update config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { structuredClone } from '@ungap/structured-clone';
 
 export default defineConfig({
   build: {
@@ -20,13 +19,13 @@ export default defineConfig({
   },
   plugins: [
     dts({
-      insertTypesEntry: true, // Optional: Automatically add a types entry to package.json
+      insertTypesEntry: true,
     }),
   ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts', // You can create a separate setup file
+    setupFiles: './vitest.setup.ts',
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
